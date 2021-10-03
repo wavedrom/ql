89e9ced46feb0773cff67851953d4a805d6c2d65
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-10-03 15:35:31 -0700

    :sparkles: extra path magic

--- test/basic.js
+++ test/basic.js
@@ -14,19 +14,19 @@ foo
   ./bar/baz
 
   dir/
-./fuz
+./_fuz42
  ../../bam
 `,
     res: [
       {kind: 'empty',  path: []},
       {kind: 'empty',  path: []},
-      {kind: 'signal', path: [], id: 'foo'},
-      {kind: 'signal', path: [], id: 'bar'},
-      {kind: 'signal', path: ['bar'], id: 'baz'},
+      {kind: 'signal', path: [],              id: 'foo'},
+      {kind: 'signal', path: [],              id: 'bar'},
+      {kind: 'signal', path: ['bar'],         id: 'baz'},
       {kind: 'empty',  path: ['bar']},
       {kind: 'empty',  path: ['bar', 'dir']},
-      {kind: 'signal', path: ['bar', 'dir'], id: 'fuz'},
-      {kind: 'signal', path: [], id: 'bam'},
+      {kind: 'signal', path: ['bar', 'dir'],  id: '_fuz42'},
+      {kind: 'signal', path: [],              id: 'bam'},
       {kind: 'empty',  path: []}
     ]
   }
